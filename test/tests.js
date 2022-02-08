67b6f0cf0860f8e70ebb0a4a6ac1632761f2c960
BrendenSeifried <brendenseifried@gmail.com>
2022-02-08 15:57:58 -0800

    updating project for grading

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,5 @@
-import { renderEnemyName } from '../utils.js';
+import './utils.test.js';
 
-const test = QUnit.test;
 
-test('enemy data test', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = '<div><img src="../assets/alive.png"><li>bob The Demon has 3 Hit points left </li></div>';
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = renderEnemyName({ enemyNme: 'bob', icon: '../assets/alive.png', HP: 3 });
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected);
-});
+
